a5a1d251f0e347a8cef36b5cd0452a1c56d7254f
95roma <30632367+95roma@users.noreply.github.com>
2019-06-18 13:10:57 -0500

    falta llamar al repsoitorio

--- registro.php
+++ registro.php
@@ -34,7 +34,8 @@
               <div class="text-center">
                 <h1 class="h4 text-gray-900 mb-4">Registro Jugador!</h1>
               </div>
-              <form class="user">
+              <form class="user" >
+                 <input type="hidden" name="pass" id="pass" >
                 <div class="form-group row">
                   <div class="col-sm-6 mb-3 mb-sm-0">
                     <input type="text" class="form-control form-control-user" id="nombre" placeholder="Nombre">
@@ -54,8 +55,9 @@
                     <input type="password" class="form-control form-control-user" id="telefono" placeholder="Telefono">
                   </div>
                 </div>
-                <a href="" class="btn btn-primary btn-user btn-block">
-                  Guardar
+                <a type="" href="" class="btn btn-primary btn-user btn-block">
+                  <button type="submit">Guardar</button>
+                  
                 </a>
                 <hr>
                 <a href="index.php" class="btn btn-google btn-user btn-block">
@@ -86,3 +88,26 @@
 </body>
 
 </html>
+<?php
+if (isset($_REQUEST["pass"])
+
+    ) {
+     
+    echo '<script>alert("No se puedo realizar el registro acivo", "Favor revisar los datos e intentar nuevamente' .  '", );</script>';
+
+    /*
+    Conexion::abrir_conexion(); 
+
+    $categoria = new Categoria();
+    $categoria->setCodigo_tipo($_REQUEST["nameApellido"]);
+    $categoria->setNombre($_REQUEST["nameNombre"]);
+
+    Repositorio_categoria::insertar_categoria(Conexion::obtener_conexion(), $categoria); 
+   */
+    //Conexion::cerrar_conexion();
+      
+        
+}
+
+
+?>
