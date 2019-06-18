fca039ccc3dc1ea4a0a418bea40e7742026f34f6
95roma <30632367+95roma@users.noreply.github.com>
2019-06-17 23:46:08 -0500

    holis

--- modelos/persona.php
+++ modelos/persona.php
@@ -1,6 +1,68 @@
 <?php 
 class persona{
-	private $
-}
+	private $dui;
+	private $nombre;
+	private $apellido;
+	private $telefono;
+	private $deporte;
+	private $edad;
+	private $equipo;
+	private $direccion;
 
+	function getDireccion(){
+		return $this->direccion;
+	}
+	function setDireccion($Direccion){
+		$this->direccion = $direccion;
+	}
+
+	function getEquipo(){
+		return $this->equipo;
+	}
+	function setEquipo($equipo){
+		$this->equipo = $equipo;
+	}
+
+	function getEdad(){
+		return $this->edad;
+	}
+	function setEdad($edad){
+		$this->edad = $edad;
+	}
+
+	function getDeporte(){
+		return $this->deporte;
+	}
+	function setDeporte($deporte){
+		$this->deporte = $deporte;
+	}
+
+	function getTelefono(){
+		return $this->telefono;
+	}
+	function setTelefono($telefono){
+		$this->telefono = $telefono;
+	}
+	function getApellido(){
+		return $this->apellido;
+	}
+	function setApellido($apellido){
+		$this->apellido = $apellido;
+	}
+
+	function getNombre(){
+		return $this->nombre;
+	}
+	function setNombre($nombre){
+		$this->nombre = $nombre;
+	}
+
+	function getDui(){
+		return $this->dui;
+	}
+	function setDui($dui){
+		$this->dui = $dui;
+	}
+
+}
 ?>
